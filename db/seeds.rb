323332feac494ebf97aef36fcd04064f2f17103e
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-22 12:01:28 -0700

    test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,85 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create([
+
+{title: "test",
+author: "test",
+genre: "test",
+image: "test", 
+publication_date: "test",
+publisher: "test",
+synopsis: "test",},
+{title: "test2",
+author:"test2",
+genre: "test2",
+image: "test2", 
+publication_date: "test2",
+publisher: "test2",
+synopsis: "test2",},
+{title: "test3",
+author: "test3",
+genre: "test3",
+image: "test3", 
+publication_date: "test3",
+publisher: "test3",
+synopsis: "test3",}
+])
+
+Movie.create([
+  {
+title: "test",
+cast: "test",
+image: "test",
+genre: "test",
+director: "test",
+premier: "test",
+language: "test",
+summary: "test"
+  },
+    {
+title: "test2",
+cast: "test2",
+image: "test2",
+genre: "test2",
+director: "test2",
+premier: "test2",
+language: "test2",
+summary: "test2"
+  },
+    {
+title: "test3",
+cast: "test3",
+image: "test3",
+genre: "test3",
+director: "test3",
+premier: "test3",
+language: "test3",
+summary: "test3"
+  }
+])
+
+Show.create([
+{
+title: "test",
+genre: "test",
+image: "test",
+premier: "test",
+summary: "test"
+},
+{
+title: "test2",
+genre: "test2",
+image: "test2",
+premier: "test2",
+summary: "test2"
+},
+{
+title: "test3",
+genre: "test3",
+image: "test3",
+premier: "test3",
+summary: "test3"
+}
+])
