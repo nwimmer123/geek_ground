deff47818ded09e9c938bae738b1b11a1f98a768
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 11:55:24 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
-  resources :users
-  resources :sessions
+  resources :users, :sessions, :book, :shows, :movies, :book_searches, :movie_searches
   get 'logout', to: "sessions#destroy"
   
 end
