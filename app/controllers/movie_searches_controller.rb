25eb70d49db75fa3b52a5f748ee360f3e0b9e7d9
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 14:59:51 -0700

    basic code

--- app/controllers/movie_searches_controller.rb
+++ app/controllers/movie_searches_controller.rb
@@ -1,2 +1,29 @@
 class MovieSearchesController < ApplicationController
+
+  include MovieSearchesHelper
+  before_action :set_user, only: [:create]
+
+    def index
+      @movie_search = MovieSearch.all
+    end
+
+    def new
+    end
+
+    def create
+      MovieSearch.delete_all
+      movie_name = params[:movie_search][:title]
+      @movie_search = BookSearch.new(movie_search_params)
+      @movie_search.data = display_results(movie_name)
+      @movie_search.save
+
+      redirect_to movie_searches_path
+    end
+
+    private
+
+      def book_search_params
+        params.require(:movie_search).permit(:data)
+      end
+
 end
--- app/helpers/movie_searches_helper.rb
+++ app/helpers/movie_searches_helper.rb
@@ -1,2 +1,22 @@
 module MovieSearchesHelper
+
+  require "net/http"
+  require "json"
+  require "openssl"
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+  SECRET_KEY = Figaro.env.the_movie_db
+
+  def movie_url(movie_name)
+    "https://api.themoviedb.org/3/movie/?api_key=#{SECRET_KEY}&language=en-US&query=#{movie_name}&page=1&include_adult=true"
+  end
+
+  def fetch_movie_data(movie_name)
+    uri = URI(book_url(movie_name))
+    response = Net::HTTP.get(uri)  
+  end
+
+  def display_results(movie_name)
+    @movie_data = fetch_movie_data(movie_name)
+  end 
 end
