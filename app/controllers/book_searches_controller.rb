d3116d59516534ba2f8a9fa4f1119ce771f0d4a1
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 14:01:35 -0700

    basic code for book api call

--- app/controllers/book_searches_controller.rb
+++ app/controllers/book_searches_controller.rb
@@ -1,2 +1,30 @@
 class BookSearchesController < ApplicationController
+
+  include BookSearchesHelper
+  before_action :set_user, only: [:create]
+
+    def index
+      @book_search = BookSearch.all
+    end
+
+    def new
+    end
+
+    def create
+      BookSearch.delete_all
+      book_name = params[:search][:title]
+      @book_search = BookSearch.new(search_params)
+      @book_search.data = display_results(book_name)
+      @book_search.save
+
+      redirect_to book_search_path
+    end
+
+    private
+
+      def search_params
+        params.require(:search).permit(:data)
+      end
+
+
 end
