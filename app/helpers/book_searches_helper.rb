0900bd2b1e3a467c792db454a724e434a7c119b0
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 12:31:16 -0700

    book search set up

--- app/helpers/book_searches_helper.rb
+++ app/helpers/book_searches_helper.rb
@@ -1,2 +1,23 @@
 module BookSearchesHelper
+
+  require "net/http"
+  require "json"
+  require "openssl"
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+  SECRET_KEY = Figaro.env.google_books
+
+  def book_url(book_name)
+    "https://www.googleapis.com/books/v1/volumes?q=#{book_name}&key=#{SECRET_KEY}"
+  end
+
+  def fetch_book_data(book_name)
+    uri = URI(book_url(book_name))
+    response = Net::HTTP.get(uri)  
+  end
+
+  def display_results(book_name)
+    @book_data = fetch_book_data(book_name)
+  end 
+  
 end
--- app/models/book_search.rb
+++ app/models/book_search.rb
@@ -1,2 +1,30 @@
 class BookSearch < ActiveRecord::Base
+
+  include BookSearchHelper
+  before_action :set_user, only: [:create]
+
+    def index
+      @search = Search.all
+    end
+
+    def new
+    end
+
+    def create
+      Search.delete_all
+      book_name = params[:search][:title]
+      @search = Search.new(search_params)
+      @search.data = display_results(book_name)
+      @search.save
+
+      redirect_to book_search_path
+    end
+
+    private
+
+      def search_params
+        params.require(:search).permit(:data)
+      end
+
+
 end
