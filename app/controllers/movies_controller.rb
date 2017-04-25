268c7eafb2532c7fc55021effd9d8945e424cc18
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 20:09:42 -0700

    movies saving

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,53 @@
 class MoviesController < ApplicationController
+
+  before_action :current_movie, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:create, :show, :update, :destroy]
+
+  def index
+    if params[:search]
+      @movies = Movie.search(params[:search]).order("created_at DESC")
+    else
+      @movies = Movie.all.order(created_at: :desc)
+    end
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to root_path
+  end
+
+  def show
+    @reviews = Review.hash_tree
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update_attributes(movie_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @reviews = Review.where(movie_id: @movie.id)
+    @reviews.each do |review|
+      review.destroy
+    end
+    @movie.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :genre, :image, :cast, :director, :premier, :language, :summary)
+  end
+
+  def current_movie
+    @movie = Movie.find_by_id(params[:id])
+  end
 end
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,4 @@
 class ShowsController < ApplicationController
+
+  
 end
