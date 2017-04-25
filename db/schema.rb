b6321f0ba590c2d0caa9bd698e20488910e96f89
Noah Wimmer <nwimmer123@yahoo.com>
2017-04-24 20:31:40 -0700

    generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425000156) do
+ActiveRecord::Schema.define(version: 20170425033119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,11 @@ ActiveRecord::Schema.define(version: 20170425000156) do
     t.string   "image"
     t.string   "premier"
     t.string   "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
